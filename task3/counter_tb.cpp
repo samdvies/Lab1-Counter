77131409949c71a8f78d925c9a36f21a04dfde0c
samdvies <daviessam003@gmail.com>
2024-10-20 21:56:23 +0100

    task3 added

---
+++ task3/counter_tb.cpp
@@ -0,0 +1,60 @@
+#include "Vcounter.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
+
+int main(int argc, char **argv, char **env) {
+    int i;
+    int clk;
+
+
+    Verilated::commandArgs(argc, argv);
+    // init top verilog instance
+    Vcounter* top = new Vcounter;
+    //init trace dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace (tfp,99);
+    tfp->open ("counter.vcd");
+
+    //init vbuddy
+    if (vbdOpen() !=1) return(-1);
+    vbdHeader("Lab 1: Counter");
+
+    //initialize
+    top->clk = 1;
+    top->rst = 1;
+    top->en = 0;
+    top->pos = 1;
+
+
+    //run simulation fpr many clock cycles
+    for (i=0; i<1000; i++) {
+        //dump variables in VCD file and toggle clock
+        for (clk=0; clk<2; clk++) {
+            tfp->dump (2*i+clk);
+            top->clk = !top->clk;
+            top->eval ();
+
+        }
+        
+        //send count value to Vbuddy
+        vbdPlot(int(top->count), 0, 255);
+
+        vbdCycle(i+1);
+        // end of Vbuddy iutput section
+
+        //change input stimuli
+        top->rst = 0;
+        top->en = 1;
+        top->pos = vbdFlag();
+        if (Verilated::gotFinish()) exit(0);
+
+    }
+
+    vbdClose();
+    tfp->close();
+    exit(0);
+    
+
+}
